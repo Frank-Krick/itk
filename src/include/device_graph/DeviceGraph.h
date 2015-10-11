2971dc5f7bc30627773124b4482879a676724535
Frank Krick <frank.krick@gmail.com>
2015-10-11 11:27:29 -0400

    Added method to get devices from device graph.

--- src/include/device_graph/DeviceGraph.h
+++ src/include/device_graph/DeviceGraph.h
@@ -15,39 +15,44 @@
 
 namespace itk {
 
-    enum class ConnectionType { AUDIO, CONTROL };
+enum class ConnectionType { AUDIO, CONTROL };
 
-    std::ostream & operator << (std::ostream & stream, const ConnectionType & type);
+std::ostream & operator << (std::ostream & stream, const ConnectionType & type);
 
-    class DeviceGraph {
-    public:
-
-        typedef std::shared_ptr<DeviceGraph> Ptr;
-
-        struct DeviceDescription {
-            std::string name;
-            IndexType deviceId = 0;
-        };
-
-        virtual ~DeviceGraph() {};
-
-        virtual IndexType addDevice(Device::Ptr device) = 0;
-        virtual void removeDevice(int deviceId) = 0;
-
-        /*
-         * Methods to manage connections between the devices.
-         */
-        virtual void connect(IndexType sourceId, IndexType targetId) = 0;
-        virtual void connect(IndexType sourceId, IndexType targetId, IndexType parameterId) = 0;
-        virtual void disconnect(IndexType sourceId, IndexType targetId) = 0;
-        virtual bool isConnected(IndexType sourceId, IndexType targetId) = 0;
-        virtual bool isConnected(IndexType sourceId, IndexType targetTd, IndexType parameterId) = 0;
-
-        virtual DeviceGraphInstance::Ptr createInstance() = 0;
-        virtual bool isInstanceUpToDate(DeviceGraphInstance &instance) = 0;
-
-        virtual DeviceDescription describeDevice(IndexType deviceId) = 0;
+class DeviceGraph {
+public:
+    struct DeviceDescription {
+        std::string name;
+        IndexType deviceId = 0;
     };
+
+    typedef std::shared_ptr<DeviceGraph> Ptr;
+    typedef std::vector<IndexType> Keys;
+    typedef std::vector<DeviceDescription> DeviceDescriptions;
+
+    virtual ~DeviceGraph() {};
+
+    virtual IndexType addDevice(Device::Ptr device) = 0;
+    virtual void removeDevice(int deviceId) = 0;
+
+    /*
+     * Methods to manage connections between the devices.
+     */
+    virtual void connect(IndexType sourceId, IndexType targetId) = 0;
+    virtual void connect(IndexType sourceId, IndexType targetId, IndexType parameterId) = 0;
+    virtual void disconnect(IndexType sourceId, IndexType targetId) = 0;
+    virtual bool isConnected(IndexType sourceId, IndexType targetId) = 0;
+    virtual bool isConnected(IndexType sourceId, IndexType targetTd, IndexType parameterId) = 0;
+
+    /*
+     * Methods to handle devices in the device graph
+     */
+    virtual DeviceDescription describeDevice(IndexType deviceId) = 0;
+    virtual DeviceDescriptions devices() = 0;
+
+    virtual DeviceGraphInstance::Ptr createInstance() = 0;
+    virtual bool isInstanceUpToDate(DeviceGraphInstance &instance) = 0;
+};
 
 }
 
